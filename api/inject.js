d3118699f28b23834b01633d77c360c100732411
NikunjP1995 <nikunjpandey22@gmail.com>
2026-04-29 10:19:23 -0400

    Delete api/inject.js

--- api/inject.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const key = process.env.SUPABASE_ANON_KEY || '';
-  res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send(key);
-}
